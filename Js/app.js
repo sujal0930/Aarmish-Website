8e4641768fc05d153b0e242e1f40b7b9d11addda
Sandesh <sandeshsachdev27@gmail.com>
2021-03-29 20:20:25 +0530

    day-3-Navbar-fixed

---
+++ Js/app.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function(){
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 20) {
+        document.getElementById('navbar-top').classList.add('fixed-top');
+         document.getElementById('navbar-top').classList.add('bg-dark');
+        // // add padding top to show content behind navbar
+        // navbar_height = document.querySelector('.navbar').offsetHeight;
+        // document.body.style.paddingTop = navbar_height + 'px';
+      } else {
+        document.getElementById('navbar-top').classList.remove('fixed-top');
+        document.getElementById('navbar-top').classList.remove('bg-dark');
+         // remove padding top from body
+        document.body.style.paddingTop = '0';
+      } 
+  });
+}); 
+
+
