e1fa003e9a40ad338d346abe9857d794d848953f
Sujal Agrwal <sujalagrawal069@gmail.com>
2021-04-03 00:34:35 +0530

    service icon change , midnight to light...night end

--- Js/app.js
+++ Js/app.js
@@ -1,23 +1,23 @@
-document.addEventListener("DOMContentLoaded", function(){
-  window.addEventListener('scroll', function() {
-      if (window.scrollY > 400) {
-        document.getElementById('navbar-top').classList.add('fixed-top');
-         
-        // // add padding top to show content behind navbar
-        // navbar_height = document.querySelector('.navbar').offsetHeight;
-        // document.body.style.paddingTop = navbar_height + 'px';
-      } else {
-        document.getElementById('navbar-top').classList.remove('fixed-top');
-        
-         // remove padding top from body
-        document.body.style.paddingTop = '0';
-      } 
-  });
-}); 
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 300) {
+            document.getElementById('navbar-top').classList.add('fixed-top');
+
+            // // add padding top to show content behind navbar
+            // navbar_height = document.querySelector('.navbar').offsetHeight;
+            // document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+            document.getElementById('navbar-top').classList.remove('fixed-top');
+
+            // remove padding top from body
+            document.body.style.paddingTop = '0';
+        }
+    });
+});
 
 
 function dropdownColor() {
-  document.getElementById('navbar-top').classList.toggle('bg-custom');
+    document.getElementById('navbar-top').classList.toggle('bg-custom');
 }
 
 
@@ -29,7 +29,7 @@ function dropdownColor() {
 //     document.getElementById('hero-text-h2').classList.add('display-3');
 // }
 // else {
-    
+
 //     document.getElementById('hero-text-h2').classList.add('display-1');
 //     document.getElementById('hero-text-h2').classList.remove('display-4');
 // }
@@ -39,4 +39,3 @@ function dropdownColor() {
 //     document.getElementById('hero-text-h2').classList.remove('display-1');
 //     document.getElementById('hero-text-h2').classList.add('display-4');
 // }
-
